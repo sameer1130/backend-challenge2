3ef4cd334f859ee03a344b7112adec96dcb516d8
Sameer Kamble <sameerkamble1130@gmail.com>
2024-09-08 21:27:00 -0400

    Added logo and toggle for dark and light mode

--- Image-uploader/src/App.tsx
+++ Image-uploader/src/App.tsx
@@ -1,15 +1,30 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import { SwitchMode } from './components/SwitchMode'
+import Logo from "./images/logo.svg"
+import LogoWhite from "./images/logo-white.svg"
+
 function App() {
-  
+  const [lightMode, setLightMode] = useState(true)
+
+  const handlerSwitch=() =>{
+    setLightMode(!lightMode)
+  }
 
   return (
     <>
+    <div className={`${lightMode ? "" : "bg-darkgray"}`}>
+
+      <div className='flex justify-between p-6 border-b items-center'>
       <div>
-        </div>
+        
+        <img src={lightMode ? Logo : LogoWhite} alt="Logo" ></img>
+      </div>
+        <SwitchMode lightMode={lightMode} handlerSwitch={handlerSwitch} />
+      </div>
+    </div>
     </>
   )
 }
--- Image-uploader/tailwind.config.js
+++ Image-uploader/tailwind.config.js
@@ -4,10 +4,23 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}"
+    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#3662E3',
+        lightblue: '#C2DAF9',
+        dark: '#121826',     
+        lightgray: '#E5E7EB',
+        white: '#FFFFFF',    
+        lightwhite: '#F9FAFB',
+        darkgray: '#212936',  
+        graytext: '#4D5562',  
+        transparentwhite: '#F9FAFBCC'
+      }
+    },
   },
   plugins: [],
 }
