09bf0649094ddde3361f8bc2f00f3464eb245f07
Sameer Kamble <sameerkamble1130@gmail.com>
2024-09-09 22:16:38 -0400

    added some functions to the buttons

--- Image-uploader/src/components/DownloadButton.tsx
+++ Image-uploader/src/components/DownloadButton.tsx
@@ -1,11 +1,24 @@
 import { Button } from "./Button"
 import Download from "../images/download.svg"
+import { ShareButton } from "./ShareButton"
+import { useState } from "react"
 
 
 export const DownloadButton = () => {
+    const [shareLink, setShareLink] = useState<string>("")
+    const handleDownload = () =>{
+        if(!shareLink){
+            alert("NO link to share");
+            return
+        }
+        const link = document.createElement('a');
+        link.href = shareLink;
+        link.download = shareLink.split("/").pop() || "download";
+        document.body.removeChild(link)
+    }
     return (
         <div>
-            <Button label="Download" image={Download} />
+            <Button label="Download" image={Download} onClick={handleDownload}/>
         </div>
     )
 }
--- Image-uploader/src/components/ShareButton.tsx
+++ Image-uploader/src/components/ShareButton.tsx
@@ -1,10 +1,39 @@
 import { Button } from "./Button"
 import Link from "../images/Link.svg"
+// import { useState } from "react"
+import {v4 as uuidv4} from "uuid";
 
 
-export const ShareButton = () => {
+export const ShareButton = ({setShareLink}:{
+    setShareLink? : (link: string)=> void
+}) => {
+    // const [shareLink, setShareLink] = useState<string>("")
+    const handleShare = () =>{
+        const uniqueId = uuidv4();  
+        const baseUrl = window.location.origin
+        const imageUrl = `${baseUrl}/path-to-image/${uniqueId}`
+
+        if(setShareLink){
+
+            setShareLink(imageUrl)
+        }
+        window.location.href = imageUrl
+        
+        if(navigator.share){
+            navigator.share({
+                title:"Share Image",
+                url: imageUrl
+            }).catch((error) => console.error("Error Sharing", error));
+        }else{
+            navigator.clipboard.writeText(imageUrl).then(() =>{
+                alert("Link copied to Clipboard")
+            })
+        }
+
+
+    }
     return (
     <div className="flex justify-center">
-        <Button label="Share" image={Link} /> 
+        <Button label="Share" image={Link} onClick={handleShare}/> 
     </div>)
 }
